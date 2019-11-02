76dc235e19ae0d47992ca072d242d0d74c53b3fa
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2019-11-02 15:50:02 -0400

    job and install scripts

---
+++ jobs/install.sh
@@ -0,0 +1,33 @@
+module purge
+
+module load gcc/7.1.0-fasrc01 hdf5/1.10.1-fasrc02
+module load python/3.6.3-fasrc02
+
+conda create -n jades python=3.6 numpy scipy matplotlib ipython
+
+# Activate
+source activate jades
+
+conda install astropy
+conda install h5py
+pip install dynesty
+
+# FSPS
+cd ~/codes/fsps
+git checkout c3k_jwst
+cd src
+make clean
+make all
+
+# python-FSPS
+cd ~/codes/python-fsps
+python setup.py install
+
+# sedpy
+cd ~/codes/sedpy
+python setup.py install
+
+# prospector
+cd ~/codes/prospector
+python setup.py install
+
---
+++ jobs/job_ody_stoch_par.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH -J stochparfit_jades
+#SBATCH -n 1 # Number of cores requested
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH -t 12:00:00 # Runtime 
+#SBATCH -p conroy,shared # Partition to submit to
+#SBATCH --mem-per-cpu=2000 # Memory per node in MB (see also --mem-per-cpu)
+#SBATCH -o /n/scratchlfs/conroy_lab/bdjohnson/jades_sed/logs/stochparfit_%A_%a.out # Standard out goes to this file
+#SBATCH -e /n/scratchlfs/conroy_lab/bdjohnson/jades_sed/logs/stochparfit_%A_%a.err # Standard err goes to this file
+
+source activate jades
+
+catalog=stochastic_mist_ckc14.h5
+lsf_file=jwst_nirspec_prism_disp.fits
+sgroup=DEEP_R100
+# Do every 10th galaxy
+declare -i objid
+objid=${SLURM_ARRAY_TASK_ID}*10
+echo $objid
+
+outdir=./output
+out=$outdir/stochastic_parametric_$objid
+
+python ./fit_stochastic_with_parametric.py \
+       --objid=$objid \
+       --add_neb --smoothssp=True --sublibres=True \
+       --lsf_file=$lsf_file \
+       --datafile=$catalog --sgroup=$sgroup \
+       --dynesty --nlive_init=200 \
+       --outfile=$out
